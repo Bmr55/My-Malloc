f56e66a6bc6a9aae7764a4fed2cc8bc5a58586e5
Benjamin Runco <bmr55@pitt.edu>
2018-02-01 16:20:05 -0500

    Create mymalloc.h

---
+++ src/mymalloc.h
@@ -0,0 +1,12 @@
+/* 
+Author: Benjamin Runco
+The header file for mymalloc.c
+*/
+
+#ifndef _MYMALLOC_H_
+#define _MYMALLOC_H_
+
+void* my_malloc(unsigned int size);
+void my_free(void* ptr);
+
+#endif
